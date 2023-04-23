5a64ad8c031075ceaad12db8ec3414e824fbb0a1
ankbhatia19 <ankbhatia19@gmail.com>
2023-04-22 18:40:14 -0700

    Added unit tests as they were accidentally ignored by gitignore

---
+++ tests/script.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+const {sum, getRandomAnswer} = require("../js/script");
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+
+test('getRandomAnswer returns a valid answer', () => {
+    const answers = [
+        "Yes", "No", "Maybe", "Ask again later", "Outlook not so good", "Cannot predict now"
+    ];
+    expect(answers).toContain(getRandomAnswer());
+});

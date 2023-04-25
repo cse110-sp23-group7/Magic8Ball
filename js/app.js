1e0aaf758071f523f91ac3b7a4a31c58f35f11a0
Ada Qi <97003885+AdaQi-001@users.noreply.github.com>
2023-04-25 16:03:22 -0700

    Create new comments in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+/** /*
+  This file contains the JavaScript code for the Magic-8-Ball application that generates random answers to user input.
+  The application uses the Document Object Model (DOM) to manipulate HTML elements on a web page and display the random answers.
+  The application includes event listeners that respond to user clicks on a "shake button" element, triggering animations and displaying answers.
+  The code is organized into functions for modularity and readability, and uses comments to provide context and explanation for each section.
+*/
+
+
+// Define an array of possible Programming Errors
 const answers = [
     `Error: Null pointer dereference`,
     `Error: Off-by-one error`,
@@ -20,10 +29,16 @@ const answers = [
     'Error: Incorrect data format',
     'Error: Improper error handling'
 ];
+
+// Get references to DOM elements in <app.html>
 const eightBall = document.querySelector('#eight-ball');
 const answer = document.querySelector('#answer');
 const shakeButton = document.querySelector('#shake-button');
+
+// Define duration of shake animation
 const shakeAnimationDuration = 1750;
+
+// When DOM is ready, add click listeners to shakeButton
 window.addEventListener("DOMContentLoaded", () => {
     // DOM ready! Images, frames, and other subresources are still downloading.
     shakeButton.addEventListener('click', getAnswer);
@@ -31,8 +46,12 @@ window.addEventListener("DOMContentLoaded", () => {
     
 });
 
+// Define shake animation
 function shake() {
+    // Add CSS class to trigger shake animation
     eightBall.classList.add('shake-animation');
+
+    //Define variables for shake animation
     const framesPerSecond = 30;
     const animationInterval = shakeAnimationDuration / (framesPerSecond * 2);
     let currentFrame = 0;
@@ -51,6 +70,8 @@ function shake() {
         'translateY(5px)',
         'translateX(0)'
     ];
+
+    // Animate the shake
     const animationIntervalId = setInterval(() => {
         eightBall.style.transform = animationFrames[currentFrame];
         currentFrame++;
@@ -61,13 +82,16 @@ function shake() {
     
 }
 
+// Get a random answer from the answers array and display it
 function getAnswer() {
     const randomIndex = Math.floor(Math.random() * answers.length);
     answer.textContent = answers[randomIndex];
 
+    // Fade in the answer
     fadeAnswerIn(0); // Start with opacity 0
 }
 
+// Recursive funtion to fade in the answer
 function fadeAnswerIn(opacity) {
     const fadeInSpeed = 0.05;
     if (opacity < 1) {
@@ -79,5 +103,5 @@ function fadeAnswerIn(opacity) {
     }
 }
 
-
+// Export an empty module (presumably for testing purposes)
 module.exports = {};
